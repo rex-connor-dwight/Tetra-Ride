65c19804c846b9487ffbc12bf8067c2c96bd9a0e
REX CONNOR DWIGHT <115325581+rex-connor-dwight@users.noreply.github.com>
2023-09-28 18:18:57 +0100

    Add files via upload

---
+++ booksummary.js
@@ -0,0 +1,28 @@
+function emailSend(){
+
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
+
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
+	Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "tetraride@gmail.com",
+    Password : "BE674FAC6AF591F3F760FB32DF43851ED4BD",
+    To : 'tetraride@gmail.com',
+    From : "tetraride@gmail.com",
+    Subject : "This is the subject",
+    Body : messageBody
+}).then(
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
+);
+} 
