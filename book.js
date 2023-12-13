0cb13e50e1e8de73f6be26351a73433bfd24bd3e
REX CONNOR DWIGHT <115325581+rex-connor-dwight@users.noreply.github.com>
2023-12-13 09:42:45 +0100

    Add files via upload

---
+++ book.js
@@ -0,0 +1,150 @@
+const userPin = document.getElementById("userPin");
+const from = document.getElementById("from");
+
+const userDes = document.getElementById("userDes");
+const torh = document.getElementById("torh");
+
+const userDate = document.getElementById("userDate");
+const nDate = document.getElementById("nDate");
+
+
+const userName = document.getElementById("userName");
+const nName = document.getElementById("nName");
+
+const userEmail = document.getElementById("userEmail");
+const nEmail = document.getElementById("nEmail");
+
+const userAddress = document.getElementById("userAddress");
+const nAddress = document.getElementById("nAddress");
+
+
+// total
+const totalAmount =  document.getElementById("totalAmount");
+
+
+
+
+
+
+
+
+
+userPin.addEventListener("input", function () {
+from.textContent = userPin.value;
+});
+
+userDes.addEventListener("input", function () {
+torh.textContent = userDes.value;
+});  
+
+userDate.addEventListener("input", function () {
+nDate.textContent = userDate.value;
+}); 
+
+
+userName.addEventListener("input", function () {
+nName.textContent = userName.value;
+}); 
+
+userEmail.addEventListener("input", function () {
+nEmail.textContent = userEmail.value;
+}); 
+
+userAddress.addEventListener("input", function () {
+nAddress.textContent = userAddress.value;
+});
+
+
+// JavaScript to show and hide the popup
+const showPopupButton = document.getElementById('showPopupButton');
+const popup = document.getElementById('popup');
+const closePopupButton = document.getElementById('closePopupButton');
+
+showPopupButton.addEventListener('click', () => {
+    popup.style.display = 'block';
+    document.body.style.overflow = 'hidden'; // Disable scrolling
+});
+
+closePopupButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling
+});
+
+
+ // Function to check if all inputs are filled
+ function checkInputs() {
+    if (userPin.value.trim() !== '' && userDes.value.trim() !== '' && userDate.value.trim() !== '') {
+        showPopupButton.removeAttribute('disabled');
+    } else {
+        showPopupButton.setAttribute('disabled', 'true');
+    }
+}
+ // Add event listeners to the input fields to check for changes
+ userPin.addEventListener('input', checkInputs);
+ userDes.addEventListener('input', checkInputs);
+ userDate.addEventListener('input', checkInputs);
+
+
+ // JavaScript to show and hide the paymenOption
+const checkOut = document.getElementById('checkOut');
+const paymentOption = document.getElementById('paymentOption');
+//const closePopupButton = document.getElementById('closePopupButton');
+
+checkOut.addEventListener('click', () => {
+    paymentOption.style.display = 'block';
+    document.body.style.overflow = 'hidden'; // Disable scrolling
+});
+
+closePopupButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling
+});
+
+//PayPal popup
+const showPayPal = document.getElementById('showPayPal');
+const payPal = document.getElementById('payPal');
+const closePayPal = document.getElementById('closePayPal');
+
+showPayPal.addEventListener('click', () => {
+    payPal.style.display = 'block';
+    document.body.style.overflow = 'hidden'; // Disable scrolling
+});
+
+closePayPal.addEventListener('click', () => {
+    payPal.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling
+});
+
+
+//applePay popup
+const showApplePay = document.getElementById('showApplePay');
+const applePay = document.getElementById('applePay');
+const closeApplePay = document.getElementById('closeApplePay');
+
+showApplePay.addEventListener('click', () => {
+    applePay.style.display = 'block';
+    document.body.style.overflow = 'hidden'; // Disable scrolling
+});
+
+closeApplePay.addEventListener('click', () => {
+    applePay.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling
+});
+
+
+//cardPay popup
+const showCardPay = document.getElementById('showCardPay');
+const cardPay = document.getElementById('cardPay');
+
+showCardPay.addEventListener('click', () => {
+    cardPay.style.display = 'block';
+    document.body.style.overflow = 'hidden'; // Disable scrolling
+});
+
+
+
+
+
+
+
+
---
+++ bookdyn.js
@@ -0,0 +1,97 @@
+{// Get references to the select element and the dynamic content div
+const userDes = document.getElementById('userDes');
+const totalAmount = document.getElementById('totalAmount');
+
+// Define a function to update the dynamic content
+function updateDynamicContent() {
+    // Get the selected option's value
+    const selectedOption = userDes.value;
+
+    // Define the content for each option
+    const optionContent = {
+        Berdiansk: "EURO 500.00",
+        option2: "You selected Option 2.",
+        option3: "You selected Option 3."
+    };
+
+    // Update the dynamic content based on the selected option
+    totalAmount.textContent = optionContent[selectedOption];
+}
+
+// Add an event listener to the select element
+totalAmount.addEventListener('change', updateDynamicContent);
+
+// Initialize the dynamic content when the page loads
+updateDynamicContent();}
+
+
+
+
+
+// New user Des
+
+
+function userDesValue(){
+
+    const userDes = document.getElementById("userDes").value;
+
+
+    const userDesPrice = {
+        "Alchevsk": "EURO 580",
+        "Berdiansk": "EURO 440",
+        "bilaTserkva": "EURO 420",
+        "Brovary": "EURO 425",
+        "Cherkasy": "EURO 540",
+        "Chernihiv": "EURO 490",
+        "Cherkasy": "EURO 450",
+        "Chernivtsi": "EURO 550",
+        "Chervonohrad": "EURO 610",
+        "Dnipro": "EURO 410",
+        "Donetsk": "EURO 413",
+        "Dovzhansk": "EURO 496",
+        "Drohobych": "EURO 435",
+        "Horlivka": "EURO 520",
+        "Ivano-Frankivsk": "EURO 560",
+        "Kamianets-Podilskyi": "EURO 590",
+        "Kerch": "EURO 490",
+        "Kamianske": "EURO 505",
+        "Kharkiv": "EURO 512",
+        "Kherson": "EURO 484",
+        "Khmelnytskyi": "EURO 444",
+        "Khrustalnyi": "EURO 496",
+        "Konotop": "EURO 543",
+        "Kramatorsk": "EURO 507",
+        "Kremenchuk": "EURO 404",
+        "Kropyvnytskyi": "EURO 503",
+        "Kryvyi-Rih": "EURO 597",
+        "Luhansk": "EURO 592",
+        "Lutsk": "EURO 543",
+        "Lviv": "EURO 587",
+        "Lysychansk": "EURO 498",
+        "Makiivka": "EURO 442",
+        "Mariupol": "EURO 497",
+        "Melitopol": "EURO 455",
+        "Mukacheve": "EURO 554",
+        "Mykolaiv": "EURO 399",
+        "Nikopol": "EURO 603",
+        "Odesa": "EURO 605",
+        "Pavlohrad": "EURO 594",
+        "Poltava": "EURO 539",
+        "Rivne": "EURO 598",
+        "Sevastopol": "EURO 599",
+        "Sievierodonetsk": "EURO 449",
+        "Simferopol": "EURO 486",
+        "Sloviansk": "EURO 533",
+        "Sumy": "EURO 477",
+        "Ternopil": "EURO 537",
+        "Uman": "EURO 459",
+        "Uzhhorod": "EURO 439",
+        "Vinnytsia": "EURO 472",
+        "Yevpatoriia": "EURO 494",
+        "Zaporizhzhia": "EURO 455",
+        "Zhytomyr": "EURO 522"
+        
+    }
+
+    document.getElementById("totalAmount").textContent =  userDesPrice[userDes];
+}
