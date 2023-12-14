d7081d09584fd6c75a09050b7fa03cbb9ca86310
REX CONNOR DWIGHT <115325581+rex-connor-dwight@users.noreply.github.com>
2023-12-14 13:09:29 +0100

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,15 @@
+
+
+    // Callback function for initializing Google Translate API
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({ pageLanguage: 'en', includedLanguages: 'es,fr,uk,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
+    }
+
+    // Load the Google Translate API
+    (function () {
+        var googleTranslateScript = document.createElement('script');
+        googleTranslateScript.type = 'text/javascript';
+        googleTranslateScript.async = true;
+        googleTranslateScript.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(googleTranslateScript);
+    })();
